40c064e4b9a1a954bebf4b0aee7982ffa4536200
kisihara_c <kisihara.c@gmail.com>
2020-01-26 14:02:56 +0900

    index.html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>countrain</title>
+    <style>
+        body{background-color:#0c0c0f;}
+
+        h1{color:#ede5bb;text-align:right;}
+
+        .train img{position:absolute;bottom:0;height:90%;max-width:90%}
+
+    </style>
+
+</head>
+
+
+<body>
+
+<h1>countrain.</h1>
+
+<div class="train"><img src= {{asset('/img/trainAndStars.png')}} ></div>
+
+
+</body>
+
+
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/countrain/', function () {
+    return view('index');
+});
