3627a5a71df6c5f2922f17fb49d31ac2f4771ad4
kisihara_c <kisihara.c@gmail.com>
2020-02-09 18:03:41 +0900

    making auth

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/countrain/', function () {
 Route::get('/countrain/first-1/', function () {
     return view('first-1');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
